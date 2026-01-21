c6dbd9bbc24280b3d5485f19387a6fd871122be4
68011128_Sahapoom <meseal.r@gmail.com>
2026-01-21 15:01:45 +0700

    p02up

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -3,7 +3,7 @@ const input = process.argv[2]
 const point = Number(input)
 
 if(Number.isNaN(point) || !input){
-  console.log('Invalid Input');
+  console.log('Invalid input');
 }
 else if(point < 0 || point > 100){
   console.log('Invalid input');
