5f3003cfe9159ab91dcc96cbb9ae30f95db247c8
68011128_Sahapoom <meseal.r@gmail.com>
2026-01-21 15:06:59 +0700

    p03

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,3 +1,22 @@
 // Write your code below
-const users = [{name: "Alice",age : 20}]
- console.log(users[0].age)
+export{}
+const inputRaw = process.argv[2];
+
+if (inputRaw === undefined || inputRaw.trim() === "" || isNaN(Number(inputRaw)) || !Number.isInteger(Number(inputRaw)) || Number(inputRaw) <= 0) {
+  console.log("Invalid input");
+  process.exit(1);
+}
+
+const input = Number(inputRaw);
+
+for (let i = 1; i < input; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+}
