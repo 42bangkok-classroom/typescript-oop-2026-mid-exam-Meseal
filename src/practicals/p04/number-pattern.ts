16e302bce1799f0e1fbf6efef41ff758ccf3986f
68011128_Sahapoom <meseal.r@gmail.com>
2026-01-21 15:46:31 +0700

    p04

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,26 @@
 // Write your code below
+export{}
+const sizeArg = process.argv[2];
+
+const size = Number(sizeArg);
+
+if (sizeArg === undefined || sizeArg.trim() === "" || isNaN(Number(sizeArg)) || !Number.isInteger(Number(sizeArg)) || Number(sizeArg) <= 0) {
+    console.log("Invalid input");
+    process.exit();
+  }
+
+for (let i = 0; i < size; i++) {
+    let result = '';
+    const startNumber = size - i;
+
+    for(let j = size - i - 1; j > 0; j--){
+        result += ' '
+    }
+
+    for (let j = 6; j > startNumber; j--) {
+        const currentNumber = j - startNumber;
+        result += currentNumber;
+    }
+    
+    console.log(result);
+}
